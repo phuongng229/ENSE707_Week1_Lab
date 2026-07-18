565587e5bb1c8102bd738783383df3c61f7134aa
phuongng229 <felice.nguyenvn@gmail.com>
2026-07-19 10:58:56 +1200

    Do Activity 6 Create Unit Test Cases

---
+++ ENSE707_Week1_Lab.Tests/BankAccountTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENSE707_Week1_Lab;
+
+namespace ENSE707_Week1_Lab_Tests;
+[TestClass]
+public class BankAccountTests
+{
+    [TestMethod]
+    public void Deposit_PositiveAmount_IncreasesBalance()
+    {
+        // Arrange
+        BankAccount account = new BankAccount("Test User", 100);
+
+        // Act
+        account.Deposit(50);
+
+        // Assert
+        Assert.AreEqual(150, account.Balance);
+    }
+
+    [TestMethod]
+    public void Withdraw_ValidAmount_DecreasesBalance()
+    {
+        // Arrange
+        BankAccount account = new BankAccount("Test User", 100);
+
+        bool result = account.Withdraw(40);
+
+        Assert.IsTrue(result);
+
+        // Assert
+        Assert.AreEqual(60, account.Balance);
+    }
+
+    [TestMethod]
+    public void Withdraw_AmountGreaterThanBalance_ReturnsFalse()
+    {
+        // Arrange
+        BankAccount account = new BankAccount("Test User", 100);
+
+        // Act
+        bool result = account.Withdraw(300);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+}
