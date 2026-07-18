0f90b9865938aa08053ef5a1670a0290c61a0d07
phuongng229 <felice.nguyenvn@gmail.com>
2026-07-19 10:52:23 +1200

    Initial Week 1 lab project setup

---
+++ ENSE707_Week1_Lab/BankAccount.cs
@@ -0,0 +1,26 @@
+namespace ENSE707_Week1_Lab;
+
+public class BankAccount
+{
+    public string AccountHolder {get; set;}
+    public decimal Balance {get; private set;}
+    public BankAccount(string accountHolder, decimal openingBalance)
+    {
+        AccountHolder = accountHolder;
+        Balance = openingBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance = Balance + amount;
+    }
+    public bool Withdraw(decimal amount)
+    {
+        Balance = Balance - amount;
+        return true;
+    }
+    public decimal CalculateTransactionFee(decimal amount)
+    {
+        return amount * 0.02m;
+    }
+}
---
+++ ENSE707_Week1_Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using ENSE707_Week1_Lab;
+
+BankAccount account = new BankAccount("Student User", 100);
+
+account.Deposit(50);
+account.Withdraw(30);
+
+Console.WriteLine($"Account holder: {account.AccountHolder}");
+Console.WriteLine($"Current holder: {account.Balance}");
+Console.WriteLine($"Fee on $100 transaction: {account.CalculateTransactionFee(100)}");
+
+
