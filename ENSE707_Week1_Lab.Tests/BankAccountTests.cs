dc3659996c81d057f678add682c6048f64fd9763
phuongng229 <felice.nguyenvn@gmail.com>
2026-07-19 12:48:38 +1200

    Add more unit tests and update MSTest and DotNet versions

--- ENSE707_Week1_Lab.Tests/BankAccountTests.cs
+++ ENSE707_Week1_Lab.Tests/BankAccountTests.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ENSE707_Week1_Lab;
 
-namespace ENSE707_Week1_Lab_Tests;
+namespace ENSE707_Week1_Lab.Tests;
 [TestClass]
 public class BankAccountTests
 {
@@ -44,4 +44,48 @@ public class BankAccountTests
         // Assert
         Assert.IsFalse(result);
     }
+
+    [TestMethod]
+    public void OpeningBalance_NegativeAmount_ThrowsArgumentException()
+    {
+        // Assert
+        Assert.ThrowsException<ArgumentException>(() => new BankAccount("Test User", -1));
+    }
+
+    [TestMethod]
+    public void AccountHolderName_NullOrEmpty_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(() => new BankAccount("", 100));
+        Assert.ThrowsException<ArgumentException>(() => new BankAccount(null, 50));
+    }
+
+    [TestMethod]
+    public void Deposit_NegativeAmount_ThrowsArgumentException()
+    {
+        // Arrange
+        BankAccount account = new BankAccount("Test User", 200);
+
+        // Assert
+        Assert.ThrowsException<ArgumentException>(() => account.Deposit(-50));
+    }
+
+    [TestMethod]
+    public void Withdraw_NegativeAmount_ThrowsArgumentException()
+    {
+        // Arrange
+        BankAccount account = new BankAccount("Test User", 200);
+
+        // Assert
+        Assert.ThrowsException<ArgumentException>(() => account.Withdraw(-50));
+    }
+
+    [TestMethod]
+    public void Withdraw_AmountGreaterThanBalance_DoesNotChangeBalance()
+    {
+        BankAccount account = new BankAccount("Test User", 200);
+
+        Assert.IsFalse(account.Withdraw(300));
+        Assert.AreEqual(200, account.Balance);
+    }
+
 }
